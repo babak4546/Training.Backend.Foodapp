1d8cf1b6b77b67eee970bbbf1c2e0fe0d849322d
Babak <babak.ares23@gmail.com>
2023-07-01 18:20:48 +0330

    start backend

---
+++ Novin.FoodApp.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novin.FoodApp.Core.Entities
+{
+    public class ApplicationUser  
+    {
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public string Type { get; set; }
+        public string Email { get; set; }
+        
+        
+    }
+}
---
+++ Novin.FoodApp.Infrastructure/Data/NovinFoodAppDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Novin.FoodApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novin.FoodApp.Infrastructure.Data
+{
+    public class NovinFoodAppDB:DbContext
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public NovinFoodAppDB(DbContextOptions<NovinFoodAppDB>options)
+            :base(options) 
+        {
+             
+        }
+    }
+}
