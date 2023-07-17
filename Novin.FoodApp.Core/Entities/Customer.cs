1c00902508a1cfb7767799d9591f447e7a6b9635
Babak <babak.ares23@gmail.com>
2023-07-17 22:31:56 +0330

    add customer

---
+++ Novin.FoodApp.Core/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novin.FoodApp.Core.Entities
+{
+    public  class Customer : BaseEntity
+    {
+        public ApplicationUser Fullname { get; set; }
+        public ApplicationUser Username { get; set;}
+
+
+
+    }
+}
